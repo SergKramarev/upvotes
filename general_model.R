126e5dad0db8fc240c396f306f490f236e0b4b86
SergKramarev <kramarevsergey@yahoo.com>
2020-01-03 17:21:24 +0200

    fully works

--- general_model.R
+++ general_model.R
@@ -2,6 +2,15 @@
 
 
 library(keras)
+library(tfruns)
+
+FLAGS <- flags(
+  flag_numeric("regularization_l2", 0.0001),
+  flag_numeric("learning_rate", 0.0001),
+  flag_numeric("regularization_l1", 0.0001)
+)
+
+
 # Inputs to model
 tag_input <- layer_input(shape = 1, name = "tag")
 reput_input <- layer_input(shape = 1, name = "reput")
@@ -10,15 +19,15 @@ other_vars_input <- layer_input(shape = 2, name = "other-vars")
 
 # Embedding layers
 tag_layer <- tag_input %>% 
-            layer_embedding(input_dim = 35, 
-                            output_dim = 25, 
+            layer_embedding(input_dim = 4, 
+                            output_dim = 2, 
                             input_length = 1, 
                             name = "tag_embedding", 
                             trainable = TRUE) %>%
             layer_flatten()
 
 reput_layer <- reput_input %>% 
-            layer_embedding(input_dim = 25,
+            layer_embedding(input_dim = 51,
                             output_dim = 25,
                             input_length = 1,
                             name = "reputation_embedding",
@@ -26,18 +35,19 @@ reput_layer <- reput_input %>%
             layer_flatten()
 
 user_layer <- user_input %>%
-            layer_embedding(input_dim = 500,
+            layer_embedding(input_dim = 48653,
                             output_dim = 50,
                             input_length = 1,
-                            name = "user_embeddings") %>%
+                            name = "user_embeddings",
+                            trainable = TRUE, batch_size = 32) %>%
             layer_flatten()
 
+
 # Concatenating model and adding more layers
-combined_model <- layer_concatenate(c(tag_layer, reput_layer, user_layer,  other_vars_input), name = "concatination_layer") %>%
-  layer_dense(80, activation = "relu") %>%
-  layer_dense(40, activation = "relu") %>%
-  layer_dense(20, activation = "relu") %>%
-  layer_dense(10, activation = "relu") %>%
+combined_model <- layer_concatenate(c(tag_layer, reput_layer, user_layer, other_vars_input), name = "concatination_layer") %>%
+  layer_dense(256, activation = "relu") %>%
+  layer_dense(128, activation = "relu") %>%
+  layer_dense(64, activation = "relu") %>%
   layer_dense(5, activation = "relu") %>%
   layer_dense(1, activation = "linear")
  
@@ -46,38 +56,28 @@ model <- keras_model(inputs = c(tag_input, reput_input, user_input, other_vars_i
 
 
 
+inputs <- list(as.matrix(as.integer(as.factor(upvote_train$Tag))),
+               as.matrix(as.integer(as.factor(upvote_train$reput_group))),
+               as.matrix(as.integer(as.factor(upvote_train$Username))),
+               as.matrix(upvote_train[, c("Answers", "Views")])
+             )
+
+opt <- optimizer_rmsprop(lr = 0.0001)
+
+model %>% 
+  compile(loss = "mse",
+                  optimizer = opt,
+                  metric = "mean_absolute_error")
+
+
+model %>%
+  fit(x = inputs,
+      y = as.matrix(upvote_train$Upvotes),
+      epochs = 100,
+      batch_size = 128)
 
 
 
+upvote_train <- arrange(as.data.frame(upvote_train), Upvotes)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-max_words <- 20
-nb_words <- 1000
-
-text_one_hot <- layer_input(nb_words)
-text_as_int <- layer_input(max_words)
-
-vec_1 <- text_one_hot %>%
-  layer_dense(100)
-
-vec_2 <- text_as_int %>% layer_embedding(
-  input_dim = nb_words, output_dim = 128, 
-  input_length = max_words
-) %>%
-  layer_lstm(128)
-
-out <- layer_concatenate(list(vec_1, vec_2))
-
-model <- keras_model(list(input_1, input_2))
